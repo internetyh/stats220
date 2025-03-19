4698a0e804825b4cdf2ef2debf73dad38079122a
internetyh <intnetyh@protonmail.com>
2025-03-19 14:29:28 +1300

    🎨 Added function for text animation for part E

--- Project1/meme.r
+++ Project1/meme.r
@@ -1,7 +1,15 @@
 library(magick)
 
+add_text <- function(image, textColor) {
+  image2 <- image %>% 
+    image_annotate("Why can't you think of a meme Mark", size = 70, gravity = "North", color = textColor) %>%
+    image_annotate("Think Mark Think!", size = 70, gravity = "South", color = textColor)
+  
+  return(image2)
+}
+
 ## |> piping makes it much easier since its all just one object anyway
-image_read("https://i.kym-cdn.com/entries/icons/facebook/000/037/158/thinkmarkthumbnail.jpg") |>
-  image_annotate("Why can't you think of a meme Mark", size=70, gravity="North", color="White") |>
-  image_annotate("Think Mark Think!", size=70, gravity="south", color="White") |>
-  image_write("my_meme.png")
+image <- image_read("https://i.kym-cdn.com/entries/icons/facebook/000/037/158/thinkmarkthumbnail.jpg")
+image <- add_text(image, "White")
+
+image %>% image_write("my_meme.png")
